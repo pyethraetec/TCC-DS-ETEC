b2d391d573d069f82578b95bd54b1d2cd362e3b4
Samuel <samuel.puci@etec.sp.gov.br>
2024-08-21 22:22:24 -0300

    JS com alguns errinhos no arq, resolver depois.

--- Trabalho prático/Projeto/Cadastro/alerta.js
+++ Trabalho prático/Projeto/Cadastro/alerta.js
@@ -1,15 +1,50 @@
-let alerta;
+let email, nome, usuario, senha, csenha;
+        email = document.getElementById("email");
+        nome = document.getElementById("nome");
+        usuario = document.getElementById("usuario");
+        senha = document.getElementById("senha");
+        csenha = document.getElementById("csenha");
 
 
 
-function alert(){
-if(alerta == true){
-alert("O cadastro foi realizado com sucesso !!")
+
+function alert22(){
+    email.style = "";
+    nome.style = "";
+    usuario.style = "";
+    senha.style = "";
+    csenha.style = "";
+  if(email.value == ""){
+    alert ("Preencha o 1° campo");
+    email.style="border:2px solid red;";
+      return false;
+
+  }
+
+else if(nome.value == ""){
+    alert ("Preencha o 2° campo");
+    nome.style="border:2px solid red;";
+    return false;
 }
 
-else(alerta == false)
 
 
-    alert("Ocorreu um erro :(")
-
+else if(usuario == ""){
+  alert ("Preencha o 3° campo");
+  usuario.style="border:2px solid red;";
 }
+
+else if(senha == ""){
+  alert ("Preencha o 4° campo");
+  senha.style="border:2px solid red;";
+}
+
+
+else if(csenha == ""){
+  alert ("Preencha o 5° campo");
+  csenha.style="border:2px solid red;";
+}
+
+
+return true;
+  }
