59f09ff14259fd9ba7da821d25ecfa72ef5e2914
unknown <Aluno@LS19M01.etecra.local>
2024-11-21 22:24:06 -0300

    a

--- Trabalho prático/Projeto/Tineabook-API_CRUD/aula/Program.cs
+++ Trabalho prático/Projeto/Tineabook-API_CRUD/aula/Program.cs
@@ -11,27 +11,57 @@ builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
 
-//
-app.MapGet("/consultaUsuarios", () =>
+
+//Cadastro Usuários
+app.MapPost("/verificaLogin", ([FromBody] JsonObject dados) =>
 {
-    MySqlConnection conexao = new MySqlConnection();
-    conexao.ConnectionString = 
-        "server=localhost;" +
-        "password= ;" +
-        "User Id=root;" +
-        "database=tineabookbd;";
-    conexao.Open();
+    // Verificando se algum campo necessário está vazio
+    if (
+        string.IsNullOrEmpty((string)dados["email"]) ||
+        string.IsNullOrEmpty((string)dados["senha"]) 
+    )
+    {
+        return Results.BadRequest(new { erro = "Campos em branco" });
+    }
 
-    MySqlCommand sql = new MySqlCommand("SELECT * FROM usuarios",conexao);
+    // String de conexão ao banco de dados
+    string connectionString = "server=localhost;password=;User Id=root;database=tineabookbd;";
 
-    DataTable dados = new DataTable();
-    dados.Load(sql.ExecuteReader());
+    using (MySqlConnection conexao = new MySqlConnection(connectionString))
+    {
+        try
+        {
+            conexao.Open();
 
-    conexao.Close();
+            MySqlCommand sql = new MySqlCommand(
+                "INSERT INTO usuarios (email, senha) " +
+                "VALUES (@email, @senha)",
+                conexao
+            );
 
-    return Results.Ok(JsonDocument.Parse(JsonConvert.SerializeObject(dados)));
+            sql.Parameters.AddWithValue("@email", dados["email"]);
+            sql.Parameters.AddWithValue("@senha", dados["senha"]);
+
+            var retorno = sql.ExecuteNonQuery();
+
+
+            if (retorno == 1)
+            {
+                return Results.Ok("Login autorizado!");
+            }
+            else
+            {
+                return Results.Problem("Erro ao logar :(");
+            }
+        }
+        catch (Exception error)
+        {
+            return Results.Problem($"Erro ao realizar o cadastro: {error.Message}");
+        }
+    }
 })
-.WithName("consultaUsuarios");
+.WithName("verificaLogin");
+
 
 
 //Cadastro Usuários
@@ -92,146 +122,6 @@ app.MapPost("/cadastrarUsuario", ([FromBody] JsonObject dados) =>
 .WithName("cadastrarUsuario");
 
 
-//Alterar informações da conta do usuário
-app.MapPost("/alterarUsuario/apelido/{id}", ([FromBody] JsonObject dados, string id) =>
-{
-    if (
-        string.IsNullOrEmpty((string)dados["Apelido"])
-    )
-    {
-        return Results.BadRequest(new { erro = "Campos em branco" });
-    }
-
-
-    // String de conexão ao banco de dados
-    string connectionString = "server=localhost;password=;User Id=root;database=tineabookbd;";
-
-    using (MySqlConnection conexao = new MySqlConnection(connectionString))
-    {
-        try
-        {
-            conexao.Open();
-            //UPDATE TABELA SET COLUNA = ""; WHERE ID = @ID; 
-            MySqlCommand sql = new MySqlCommand("UPDATE usuarios SET apelido = @apelido WHERE id = @id", conexao);
-            sql.Parameters.AddWithValue("@apelido", dados["Apelido"]);
-            sql.Parameters.AddWithValue("@id", id);
-
-            var retorno = sql.ExecuteNonQuery();
-
-            if (retorno == 1)
-            {
-                return Results.Ok("Cadastro realizado com sucesso!");
-            }
-            else
-            {
-                return Results.Problem("Oh não! Seu cadastro deu errado :(");
-            }
-        }
-        catch (Exception error)
-        {
-            return Results.Problem($"Erro ao realizar o cadastro: {error.Message}");
-        }
-    }
-})
-.WithName("alterarUsuarioApelido");
-
-
-//Alterar informações da conta do usuário
-app.MapPost("/alterarUsuario/bio/{idUsuario}", ([FromBody] JsonObject dados, string idUsuario) =>
-{
-    if (
-        string.IsNullOrEmpty((string)dados["bio"])
-    )
-    {
-        return Results.BadRequest(new { erro = "Campos em branco" });
-    }
-
-
-    // String de conexão ao banco de dados
-    string connectionString = "server=localhost;password=;User Id=root;database=tineabookbd;";
-
-    using (MySqlConnection conexao = new MySqlConnection(connectionString))
-    {
-        try
-        {
-            conexao.Open();
-            //UPDATE TABELA SET COLUNA = ""; WHERE ID = @ID; 
-            MySqlCommand sql = new MySqlCommand("UPDATE usuarios SET bio = @bio WHERE id = @id", conexao);
-            sql.Parameters.AddWithValue("@bio", dados["Bio"]);
-            sql.Parameters.AddWithValue("@id", idUsuario);
-
-            var retorno = sql.ExecuteNonQuery();
-
-            if (retorno == 1)
-            {
-                return Results.Ok("Cadastro realizado com sucesso!");
-            }
-            else
-            {
-                return Results.Problem("Oh não! Seu cadastro deu errado :(");
-            }
-        }
-        catch (Exception error)
-        {
-            return Results.Problem($"Erro ao realizar o cadastro: {error.Message}");
-        }
-    }
-})
-.WithName("alterarUsuarioBio");
-
-
-app.MapDelete("/excluirUsuario/{idUsuario}", ([FromBody] JsonObject dados, string id) =>
-{
-    MySqlConnection conexao = new MySqlConnection();
-    conexao.ConnectionString = "server=localhost;password=aluno.etec;User Id=aluno;database=escola;";
-    conexao.Open();
-
-    MySqlCommand sql = new MySqlCommand("DELETE FROM aluno WHERE id = @id", conexao);
-    sql.Parameters.AddWithValue("@id", id);
-
-    var retorno = sql.ExecuteNonQuery();
-
-    conexao.Close();
-
-    if (retorno == 1)
-    {
-        return Results.Ok();
-    }
-    else
-    {
-        return Results.Problem();
-    }
-
-})
-.WithName("excluirUsuario");
-
-
-
-
-
-//LIVROS
-
-app.MapGet("/consultaLivros", () =>
-{
-    MySqlConnection conexao = new MySqlConnection();
-    conexao.ConnectionString =
-        "server=localhost;" +
-        "password= ;" +
-        "User Id=root;" +
-        "database=tineabookbd;";
-
-    conexao.Open();
-
-    MySqlCommand sql = new MySqlCommand("SELECT * FROM livros", conexao);
-
-    DataTable dados = new DataTable();
-    dados.Load(sql.ExecuteReader());
-
-    conexao.Close();
-
-    return Results.Ok(JsonDocument.Parse(JsonConvert.SerializeObject(dados)));
-})
-.WithName("consultaLivros");
 
 
 //Cadastro Usuários
@@ -315,15 +205,14 @@ app.MapPost("/criarResenha", ([FromBody] JsonObject dados ) =>
             conexao.Open();
 
             MySqlCommand sql = new MySqlCommand(
-                "INSERT INTO avaliacao (id_usuario, id_livro, data, status, estrelas, resenha) " +
-                "VALUES (@id_usuario, @id_livro, @data, @status, @estrelas, @resenha)",
+                "INSERT INTO avaliacao (id_usuario, id_livro, data, estrelas, resenha) " +
+                "VALUES (@id_usuario, @id_livro, @data, @estrelas, @resenha)",
             conexao
             );
 
             sql.Parameters.AddWithValue("@id_usuario", dados["id_usuario"]);
             sql.Parameters.AddWithValue("@id_livro", dados["id_livro"]);
             sql.Parameters.AddWithValue("@data", dados["data"]);
-            sql.Parameters.AddWithValue("@status", dados["status"]);
             sql.Parameters.AddWithValue("@estrelas", dados["estrelas"]);
             sql.Parameters.AddWithValue("@resenha", dados["resenha"]);
 
@@ -345,6 +234,57 @@ app.MapPost("/criarResenha", ([FromBody] JsonObject dados ) =>
     }
 })
 .WithName("criarResenha");
+
+
+app.MapPost("/addHistorico", ([FromBody] JsonObject dados) =>
+{
+    if (
+        string.IsNullOrEmpty((string)dados["id_usuario"]) ||
+        string.IsNullOrEmpty((string)dados["id_livro"]) ||
+        string.IsNullOrEmpty((string)dados["data"]) ||
+        string.IsNullOrEmpty((string)dados["pagsLidas"])
+    )
+    {
+        return Results.BadRequest(new { erro = "Campos em branco" });
+    }
+
+    string connectionString = "server=localhost;password=;User Id=root;database=tineabookbd;";
+
+    using (MySqlConnection conexao = new MySqlConnection(connectionString))
+    {
+        try
+        {
+            conexao.Open();
+
+            MySqlCommand sql = new MySqlCommand(
+                "INSERT INTO avaliacao (id_usuario, id_livro, data, pagsLidas) " +
+                "VALUES (@id_usuario, @id_livro, @data, @pagsLidas)",
+            conexao
+            );
+
+            sql.Parameters.AddWithValue("@id_usuario", dados["id_usuario"]);
+            sql.Parameters.AddWithValue("@id_livro", dados["id_livro"]);
+            sql.Parameters.AddWithValue("@data", dados["data"]);
+            sql.Parameters.AddWithValue("@status", dados["pagsLidas"]);
+
+            var retorno = sql.ExecuteNonQuery();
+
+            if (retorno == 1)
+            {
+                return Results.Ok("Resenha realizada com sucesso!");
+            }
+            else
+            {
+                return Results.Problem("Algo deu errado :(");
+            }
+        }
+        catch (Exception error)
+        {
+            return Results.Problem($"Erro ao realizar a resenha: {error.Message}");
+        }
+    }
+})
+.WithName("addHistorico");
 
 
 
